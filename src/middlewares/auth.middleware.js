f283be0bdf148bad9ec60d9f8e6048f47259315e
Smt05ydv <syadav02391@gmail.com>
2026-07-27 00:21:01 +0530

    role based permisssion

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,10 @@
 import { User } from "../models/user.models.js";
+import { ProjectMember } from "../models/projectmember.models.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
 import jwt from "jsonwebtoken";
+import { ApiResponse } from "../utils/api-response.js";
+import mongoose from "mongoose";
 
  export const verifyJWT = asyncHandler(async(req,res,next)=>{
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
@@ -26,3 +29,30 @@ import jwt from "jsonwebtoken";
     
     
 });
+
+export const validateProjectPermission= (roles=[]) => {
+ asyncHandler (async(req,res,next)=>{
+    const {projectId} = req.params;
+
+    if (!projectId){
+        throw new ApiError(400,"project id is missing")
+        }
+       const project = await ProjectMember.findOne({
+            project : new mongoose.Types.ObjectId(projectId),
+             user : new mongoose.Types.ObjectId(req.user._id),
+});
+  if (!project){
+        throw new ApiError(400,"project is missing")
+        }
+
+        const givenRole= project?.role
+         req.user.role= givenRole
+
+         if (!roles.includes(givenRole)){
+            throw new ApiError(403,"You do not have permission to perform this action")
+         }
+
+         next()
+        
+ })
+}
