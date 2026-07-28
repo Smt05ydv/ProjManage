378bd37716a77d5dcf3d3a66a6625e822ecdf6dc
Smt05ydv <syadav02391@gmail.com>
2026-07-29 01:16:23 +0530

    routes for project done

---
+++ src/routes/project.routes.js
@@ -0,0 +1,61 @@
+import { Router } from "express";
+import {
+  getProject,
+    getProjectById,
+    createProject,
+    updateProject,
+    deleteProject,
+    addMembersToProject,
+    getProjectMembers,
+    updateMemberRole,
+    deleteMember,
+} from "../controllers/project.controllers.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import {
+ addMemberToProjectValidator,createProjectValidator
+} from "../validators/index.js";
+import { verifyJWT,validateProjectPermission } from "../middlewares/auth.middleware.js";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
+
+const router = Router();
+router.use(verifyJWT)
+
+router
+   .route("/")
+   .get(getProject)
+   .post(createProjectValidator(),validate,createProject)
+
+router
+   .route("/:projectId")
+   .get(validateProjectPermission(AvailableUserRole),getProjectById)
+   .put(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    createProjectValidator(),
+    validate,
+    updateProject
+   ) 
+   .delete(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    deleteProject);
+
+router
+   .route("/:projectId/members")
+   .get(getProjectMembers)
+   .post(
+     validateProjectPermission([UserRolesEnum.ADMIN]),
+     addMemberToProjectValidator(),
+     validate,
+     addMembersToProject
+   )   
+
+
+   router
+   .route("/:projectId/members/:userId")
+   .put(validateProjectPermission([UserRolesEnum.ADMIN]),updateMemberRole)
+   .delete(validateProjectPermission([UserRolesEnum.ADMIN]),deleteMember)
+
+    
+   
+    
+
+export default router
