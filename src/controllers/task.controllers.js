bf6efacc794c8a7423b1175987c64b51d48e996e
Smt05ydv <syadav02391@gmail.com>
2026-07-31 01:22:01 +0530

    task crud done

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -7,13 +7,64 @@ import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import mongoose from "mongoose";
 import { AvailableUserRole } from "../utils/constants.js";
+import { url } from "inspector";
 
 const getTasks= asyncHandler(async(req,res)=>{
     //chai
+     const{projectId} = req.params;
+    const project= await Project.findById(projectId)
+
+    if (!project){
+        throw new ApiError(404,"project not found");
+    }
+
+    const tasks= await Task.find({
+        project: new mongoose.Types.ObjectId(projectId)
+    }).populate("assignedTo", "avatar username fullname");
+
+    return res
+           .status(201)
+           .json(
+            new ApiResponse(201,tasks,"Tasks fetched successfully") );
+
+    
 });
 
 const createTasks= asyncHandler(async(req,res)=>{
     //chai
+    const {title,description,assignedTo,status}= req.body
+    const{projectId} = req.params;
+    const project= await Project.findById(projectId)
+
+    if (!project){
+        throw new ApiError(404,"project not found");
+    }
+
+    const files= req.files || []
+    const attachments = files.map((file)=>{
+        return {
+            url : `${process.env.SERVER_URL}/images/${file.originalname}`,
+             mimetype:file.mimetype,
+             size: file.size
+             }
+
+    })
+    const task = await Task.create({
+        title,
+        description,
+        project : new mongoose.Types.ObjectId(projectId),
+        assignedTo : assignedTo? new mongoose.Types.ObjectId(assignedTo) : undefined,
+        status,
+        assignedBy :new mongoose.Types.ObjectId(req.user._id),
+        attachments
+    });
+
+    return res
+           .status(201)
+           .json(
+            new ApiResponse(201,task,"Task created successfully")
+
+           )
 });
 
 const getTaskById= asyncHandler(async(req,res)=>{
